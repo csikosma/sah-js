1df4b2722cce80d5ee1503009ad5aedf5729b49b
csikosma <mate.csikos1986@gmail.com>
2020-05-11 16:58:41 +0200

    first part

---
+++ scripts.js
@@ -0,0 +1,47 @@
+console.log("most jön a java");
+let color = ["red","blue","green"];
+let post = {
+    title: "My Post",
+    description: "first",
+    likeCount: 12,
+    comment: ["cool", "nice"]
+}; 
+console.log(color);
+console.log(post);
+
+let posts = [{
+  title: "First",
+  likeCount: 12,
+}, {
+  title: "second",
+  likeCount: 222,
+}]
+let age = 33;
+
+if (age >= 18) {
+  console.log("ön felnőt");
+} else {
+  console.log("ön gyerek");  
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log (color);
+
+let myFunction = x => {
+  console.log(x);
+}
+
+let greeter = name => {
+  console.log('Hello ' + name)
+}
+
+greeter("Mate");
+
+color.forEach(col => {
+  console.log(col);
+});
+
+console.log("ez a vége");
